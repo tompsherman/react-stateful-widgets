acc4289b7c918ad306040a6f19fab05ed8e0f5a7
Tom Sherman <tompsherman@gmail.com>
2020-09-08 07:15:58 -0400

    spinner

--- src/components/Programmers.js
+++ src/components/Programmers.js
@@ -11,7 +11,7 @@ We can only feature one awesome programmer at a time.
 Find comments below to help you along.
 */
 
-import React from 'react';
+import React, { useState } from 'react';
 
 // Use this variable ONLY to initialize a slice of state!
 // There is something in the JSX right now breaking this rule...
@@ -28,6 +28,10 @@ export default function Programmers() {
   // We'll have to use the state hook twice, as we need two slices of state.
   // The programmers list on the one hand, and the id of the featured programmer on the other.
 
+  const [programmerList, setProgrammerList] = useState(listOfAwesome)
+
+  const [featuredProgrammer, setFeaturedProgrammers] = useState(null)
+
   const getNameOfFeatured = () => {
     // Leave this for last!
     // This is NOT an event handler but a helper function. See its usage inside the JSX.
@@ -39,7 +43,7 @@ export default function Programmers() {
   const style = {
     fontSize: '1.5em',
     marginTop: '0.5em',
-    color: 'royalblue', // 🤔 color turns to gold, when celebrating
+    color: featuredProgrammer === null ? 'royalblue' : "gold" // 🤔 color turns to gold, when celebrating
   };
 
   return (
--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -46,20 +46,22 @@ export default function Spinner() {
 
   const toggleSpinner = () => {
     /* STEP 4 */
+    setSpinnerOn(!spinnerOn);
   };
 
   return (
     <div className="widget-spinner container">
       <h2>Spinner</h2>
       {
-        true && (
+        spinnerOn && (
           <div id="spinner" className="spinner">
             --+--
           </div>
         ) /* STEP 2 */
       }
       <button id="toggleSpinner" onClick={toggleSpinner}>
-        Hide Spinner {/* STEP 3 */}
+        {spinnerOn === true ? "Hide Spinner" : "Show Spinner"} 
+        {/* STEP 3 */}
       </button>
     </div>
   );
