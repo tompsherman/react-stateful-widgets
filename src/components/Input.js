aa2eb6a7f081d87406085f51e2fc006b7e4e1546
Tom Sherman <tompsherman@gmail.com>
2020-09-07 22:03:04 -0400

    input

--- src/components/Input.js
+++ src/components/Input.js
@@ -46,24 +46,28 @@ export default function Input() {
     // Log out the synthetic event object 'evt' and see for yourself.
     const { value } = evt.target;
 
+    setInputValue(value);
     /* STEP 4 */
   };
   const reset = () => {
     /* STEP 5 */
+    setInputValue("")
   };
-
   const style = {
     fontSize: "1.5em",
     marginBottom: "0.3em",
-    color: inputValue > 10 ? "crimson" : "royalblue" /* STEP 2 */,
+    color: inputValue.length > 10 ? "crimson" : "royalblue" /* STEP 2 */,
   };
 
   return (
     <div className="widget-input container">
       <h2>Input</h2>
-    <div id="output" style={style}>{inputValue.toUpperCase()}</div> {/* STEP 3 */}
+      <div id="output" style={style}>
+        {inputValue.toUpperCase()}
+      </div>{" "}
+      {/* STEP 3 */}
       <div>
-        <input id="input" type="text" onChange={changeInput} /> {/* STEP 6 */}
+        <input id="input" type="text" onChange={changeInput} value = {inputValue}/> {/* STEP 6 */}
         <button id="resetInput" onClick={reset}>
           Reset
         </button>
